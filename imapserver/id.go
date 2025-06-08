5de757fa4fb8befab8d1762db8a88c92df5bcbaf
Dejan Strbac <dejan@mercata.com>
2025-06-08 18:53:35 +0200

    id fix

--- imapserver/id.go
+++ imapserver/id.go
@@ -67,7 +67,9 @@ func (c *Conn) handleID(tag string, dec *imapwire.Decoder) error {
 		enc.Special(')')
 	}
 
-	if err := enc.CRLF(); err != nil {
+	err = enc.CRLF()
+	enc.end()
+	if err != nil {
 		return err
 	}
 
